c997aae0c7962799e9a359b7aa93f9857f3a2ba8
Gabriel Omokpo <Gabriel.Omokpo@investment-one.com>
2021-09-23 13:38:31 +0100

    returned null instead of -All inp1pointEqualsP2pointAndP1pointLTFour

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -1,133 +1,104 @@
+import static org.junit.Assert.*;
 
-public class TennisGame2 implements TennisGame {
-    public int p1point = 0;
-    public int p2point = 0;
+import java.util.Arrays;
+import java.util.Collection;
 
-    public String p1res = "";
-    public String p2res = "";
-    private String player1Name;
-    private String player2Name;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-    public TennisGame2(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+@RunWith(Parameterized.class)
+public class TennisTest {
+
+    private int player1Score;
+    private int player2Score;
+    private String expectedScore;
+
+    public TennisTest(int player1Score, int player2Score, String expectedScore) {
+        this.player1Score = player1Score;
+        this.player2Score = player2Score;
+        this.expectedScore = expectedScore;
     }
 
-    public String getScore() {
-        String score = "";
-        if (p1point == p2point) {
-            if (p1point < 3) {
-                score = p1pointEqualsP2pointAndP1pointLTFour(p1point);
-            } else {
-                score = "Deuce";
-            }
-        }
+    @Parameters
+    public static Collection<Object[]> getAllScores() {
+        return Arrays.asList(new Object[][] {
+                { 0, 0, "Love-All" },
+                { 1, 1, "Fifteen-All" },
+                { 2, 2, "Thirty-All"},
+                { 3, 3, "Deuce"},
+                { 4, 4, "Deuce"},
 
-        if (p1point > 0 && p2point == 0) {
-            p1res = pointGTZeroAndPointEqualsZero(p1point);
-            p2res = "Love";
-            score = p1res + "-" + p2res;
-        }
+                { 1, 0, "Fifteen-Love"},
+                { 0, 1, "Love-Fifteen"},
+                { 2, 0, "Thirty-Love"},
+                { 0, 2, "Love-Thirty"},
+                { 3, 0, "Forty-Love"},
+                { 0, 3, "Love-Forty"},
+                { 4, 0, "Win for player1"},
+                { 0, 4, "Win for player2"},
 
-        if (p2point > 0 && p1point == 0) {
-            p2res = pointGTZeroAndPointEqualsZero(p2point);
-            p1res = "Love";
-            score = p1res + "-" + p2res;
-        }
+                { 2, 1, "Thirty-Fifteen"},
+                { 1, 2, "Fifteen-Thirty"},
+                { 3, 1, "Forty-Fifteen"},
+                { 1, 3, "Fifteen-Forty"},
+                { 4, 1, "Win for player1"},
+                { 1, 4, "Win for player2"},
 
-        if (p1point > p2point && p1point < 4) {
-            String[] pres = pointGTAndpointLTFour(p1point, p2point);
-            p1res = pres[0];
-            p2res = pres[1];
-            score = p1res + "-" + p2res;
-        }
-        if ((p2point > p1point) && p2point < 4) {
-            String[] pres = pointGTAndpointLTFour(p2point, p1point);
-            p2res = pres[0];
-            p1res = pres[1];
-            score = p1res + "-" + p2res;
-        }
+                { 3, 2, "Forty-Thirty"},
+                { 2, 3, "Thirty-Forty"},
+                { 4, 2, "Win for player1"},
+                { 2, 4, "Win for player2"},
 
-        if (p1point > p2point && p2point >= 3) {
-            score = "Advantage player1";
-        }
+                { 4, 3, "Advantage player1"},
+                { 3, 4, "Advantage player2"},
+                { 5, 4, "Advantage player1"},
+                { 4, 5, "Advantage player2"},
+                { 15, 14, "Advantage player1"},
+                { 14, 15, "Advantage player2"},
 
-        if (p2point > p1point && p1point >= 3) {
-            score = "Advantage player2";
-        }
-
-        if ((p1point >= 4) && (p2point >= 0) && (p1point - p2point) >= 2) {
-            score = "Win for player1";
-        }
-        if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2) {
-            score = "Win for player2";
-        }
-        return score;
+                { 6, 4, "Win for player1"},
+                { 4, 6, "Win for player2"},
+                { 16, 14, "Win for player1"},
+                { 14, 16, "Win for player2"},
+        });
     }
 
-    public void SetP1Score(int number) {
-        for (int i = 0; i < number; i++) {
-            p1Score();
+    public void checkAllScores(TennisGame game) {
+        int highestScore = Math.max(this.player1Score, this.player2Score);
+        for (int i = 0; i < highestScore; i++) {
+            if (i < this.player1Score)
+                game.wonPoint("player1");
+            if (i < this.player2Score)
+                game.wonPoint("player2");
         }
+        assertEquals(this.expectedScore, game.getScore());
     }
 
-    public void SetP2Score(int number) {
-        for (int i = 0; i < number; i++) {
-            p2Score();
-        }
+    @Test
+    public void checkAllScoresTennisGame1() {
+        TennisGame1 game = new TennisGame1("player1", "player2");
+        checkAllScores(game);
     }
 
-    public void p1Score() {
-        p1point++;
+    @Test
+    public void checkAllScoresTennisGame2() {
+        TennisGame2 game = new TennisGame2("player1", "player2");
+        checkAllScores(game);
     }
 
-    public void p2Score() {
-        p2point++;
+    @Test
+    public void checkAllScoresTennisGame3() {
+        TennisGame3 game = new TennisGame3("player1", "player2");
+        checkAllScores(game);
     }
 
-    public void wonPoint(String player) {
-        if (player == "player1") {
-            p1Score();
-        } else {
-            p2Score();
-        }
+    @Test
+    public void checkAllScoresTennisGame4() {
+        TennisGame game = new TennisGame4("player1", "player2");
+        checkAllScores(game);
     }
 
-    // the point can only be either 0, 1 or 2, hence the use of return to save execution time
-    private String p1pointEqualsP2pointAndP1pointLTFour(int p1point) {
-        switch (p1point) {
-            case 0: return "Love-All";
-            case 1: return "Fifteen-All";
-            case 2: return "Thirty-All";
-//            default: throw new IllegalArgumentException("p1oint value not matched");
-        }
-        return "-All";
-    }
-
-    private String pointGTZeroAndPointEqualsZero(int point) {
-        switch (point) {
-            case 1: return "Fifteen";
-            case 2: return "Thirty";
-            case 3: return "Forty";
-//            default: throw new IllegalArgumentException("p1oint value not matched");
-        }
-        return "-All";
-    }
-
-    private String[] pointGTAndpointLTFour(int p1point, int p2point) {
-        String[] pres = new String[2];
-        if (p1point == 1)
-            pres[0] = "Fifteen";
-        if (p1point == 2)
-            pres[0] = "Thirty";
-        if (p1point == 3)
-            pres[0] = "Forty";
-        if (p2point == 0)
-            pres[1] = "Love";
-        if (p2point == 1)
-            pres[1] = "Fifteen";
-        if (p2point == 2)
-            pres[1] = "Thirty";
-        return pres;
-    }
 }
